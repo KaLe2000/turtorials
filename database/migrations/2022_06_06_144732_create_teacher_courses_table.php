16b201f5801a19719824bc5ddf38d8219e38a014
Nikita Talovskiy <40030943+KaLe2000@users.noreply.github.com>
2022-06-07 10:11:36 +0700

    renamed disciplines to courses (#1)

--- database/migrations/2022_06_06_144732_create_teacher_courses_table.php
+++ database/migrations/2022_06_06_144732_create_teacher_courses_table.php
@@ -10,21 +10,21 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('teacher_disciplines', static function (Blueprint $table) {
+        Schema::create('teacher_courses', static function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('teacher_id');
-            $table->unsignedBigInteger('discipline_id');
+            $table->unsignedBigInteger('course_id');
             $table->unsignedBigInteger('city_id');
             $table->timestamps();
 
             $table->foreign('teacher_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('discipline_id')->references('id')->on('disciplines')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('teacher_disciplines');
+        Schema::dropIfExists('teacher_courses');
     }
 };
